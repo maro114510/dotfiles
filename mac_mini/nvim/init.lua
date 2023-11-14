a142863070368769d268d1319ebaa54e9cff55ef
maro114510 <atumaro1144510@icloud.com>
2023-11-14 23:35:10 +0900

    色々変更

--- mac_mini/nvim/init.lua
+++ mac_mini/nvim/init.lua
@@ -1,5 +1,20 @@
-# morimori
+vim.cmd([[
+  let &t_SI = "\<Esc>[5 q"
+  let &t_EI = "\<Esc>[2 q"
+  autocmd VimLeave * let &t_me = "\<Esc>[4 q"
+]])
 vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd([[
+    augroup RestoreCursorShapeOnExit
+        autocmd!
+        autocmd VimLeave * set guicursor=a:hor1
+    augroup END
+]])
+vim.api.nvim_set_keymap('n', 'x', '"_x', {noremap = true})
+vim.api.nvim_set_keymap('n', 's', '"_s', {noremap = true})
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
+-- vim.o.guicursor = ''
+vim.g.NERDTreeShowHidden = 1
 vim.g.tokyonight_night_comment = "#00bfff"
 
 require("options")
