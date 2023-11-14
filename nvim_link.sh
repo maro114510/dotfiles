ce8223daeec140974ec7e8e9ef73765697613f22
maro114510 <atumaro114510@icloud.com>
2023-11-15 01:43:07 +0900

    ok

---
+++ nvim_link.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+TARGET_DIR="$CURRENT_DIR/mac_mini/nvim"
+
+NVIM_DIR="$HOME/.config/nvim"
+if [ -d "$NVIM_DIR" ]; then
+    BACKUP_DIR="$HOME/.config/nvim_backup_$(date +"%Y%m%d_%H%M%S")"
+    mv "$NVIM_DIR" "$BACKUP_DIR"
+    echo "backup done"
+fi
+
+mkdir -p "NVIM_DIR"
+
+for item in "$TARGET_DIR"/*; do
+    if [ -e "$item" ]; then
+        ln -sfn "$item" "$NVIM_DIR/$(basename "$item")"
+    fi
+done
+
+echo "linked"
