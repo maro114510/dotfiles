36abf24b7fd0fe4586cc95474bcba05d491c17d8
maro114510 <atumaro114510@icloud.com>
2023-11-16 23:42:21 +0900

    2023/11/16 23:42:21 Save Work

---
+++ zsh_link.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+
+if [ -f "$HOME/.zshrc" ]; then
+    BACKUP="$HOME/.zshrc_$(date +"%Y%m%d_%H%M%S")"
+    mv "$HOME/.zshrc" "$BACKUP"
+    echo "backup done"
+fi
+
+ln -sfn "$CURRENT_DIR/.zshrc" "$HOME/.zshrc"
+
