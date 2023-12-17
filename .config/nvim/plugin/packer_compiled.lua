d497f8cb59c4e40391b7adac3f9583c2886c79fd
maro114510 <atumaro114510@icloud.com>
2023-12-18 01:51:31 +0900

    監視対象を外す

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/nohira/.cache/nvim/packer_hererocks/2.1.1700008891/share/lua/5.1/?.lua;/Users/nohira/.cache/nvim/packer_hererocks/2.1.1700008891/share/lua/5.1/?/init.lua;/Users/nohira/.cache/nvim/packer_hererocks/2.1.1700008891/lib/luarocks/rocks-5.1/?.lua;/Users/nohira/.cache/nvim/packer_hererocks/2.1.1700008891/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/nohira/.cache/nvim/packer_hererocks/2.1.1700008891/lib/lua/5.1/?.so"
+local package_path_str = "/Users/atsuki/.cache/nvim/packer_hererocks/2.1.1700008891/share/lua/5.1/?.lua;/Users/atsuki/.cache/nvim/packer_hererocks/2.1.1700008891/share/lua/5.1/?/init.lua;/Users/atsuki/.cache/nvim/packer_hererocks/2.1.1700008891/lib/luarocks/rocks-5.1/?.lua;/Users/atsuki/.cache/nvim/packer_hererocks/2.1.1700008891/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/atsuki/.cache/nvim/packer_hererocks/2.1.1700008891/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,152 +76,213 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-cmdline"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/cmp-cmdline",
     url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["coc.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/coc.nvim",
     url = "https://github.com/neoclide/coc.nvim"
+  },
+  ["denops.vim"] = {
+    loaded = true,
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/denops.vim",
+    url = "https://github.com/vim-denops/denops.vim"
   },
   fzf = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/fzf",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/fzf",
     url = "https://github.com/junegunn/fzf"
   },
   ["fzf-lua"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/fzf-lua",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/fzf-lua",
     url = "https://github.com/ibhagwan/fzf-lua"
   },
   ["fzf.vim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/fzf.vim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/fzf.vim",
     url = "https://github.com/junegunn/fzf.vim"
+  },
+  ["gin.vim"] = {
+    loaded = true,
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/gin.vim",
+    url = "https://github.com/lambdalisue/gin.vim"
   },
   ["im-select.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/im-select.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/im-select.nvim",
     url = "https://github.com/keaising/im-select.nvim"
+  },
+  ["lexima.vim"] = {
+    loaded = true,
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/lexima.vim",
+    url = "https://github.com/cohama/lexima.vim"
+  },
+  ["lspsaga.nvim"] = {
+    config = { "\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\flspsaga\frequire\0" },
+    load_after = {},
+    loaded = true,
+    needs_bufread = false,
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/opt/lspsaga.nvim",
+    url = "https://github.com/nvimdev/lspsaga.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
+  ["markdown-preview.nvim"] = {
+    loaded = true,
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim",
+    url = "https://github.com/iamcco/markdown-preview.nvim"
   },
   ["mini.indentscope"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/mini.indentscope",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/mini.indentscope",
     url = "https://github.com/echasnovski/mini.indentscope"
   },
   ["neo-tree.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/neo-tree.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/neo-tree.nvim",
     url = "https://github.com/nvim-neo-tree/neo-tree.nvim"
   },
   nerdtree = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/nerdtree",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/nerdtree",
     url = "https://github.com/preservim/nerdtree"
   },
   ["nui.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/nui.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/nui.nvim",
     url = "https://github.com/MunifTanjim/nui.nvim"
   },
   ["nvim-autopairs"] = {
     config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["tabline.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/tabline.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/tabline.nvim",
     url = "https://github.com/kdheepak/tabline.nvim"
+  },
+  ["toggleterm.nvim"] = {
+    config = { "\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0" },
+    loaded = true,
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
   ["vim-colorscheme-icons"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/vim-colorscheme-icons",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/vim-colorscheme-icons",
     url = "https://github.com/bryanmylee/vim-colorscheme-icons"
   },
   ["vim-commentary"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/vim-commentary",
     url = "https://github.com/tpope/vim-commentary"
   },
   ["vim-devicons"] = {
     loaded = true,
-    path = "/Users/nohira/.local/share/nvim/site/pack/packer/start/vim-devicons",
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/start/vim-devicons",
     url = "https://github.com/ryanoasis/vim-devicons"
+  },
+  ["vim-matchup"] = {
+    after_files = { "/Users/atsuki/.local/share/nvim/site/pack/packer/opt/vim-matchup/after/plugin/matchit.vim" },
+    loaded = true,
+    needs_bufread = true,
+    path = "/Users/atsuki/.local/share/nvim/site/pack/packer/opt/vim-matchup",
+    url = "https://github.com/andymass/vim-matchup"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Setup for: vim-matchup
+time([[Setup for vim-matchup]], true)
+try_loadstring("\27LJ\2\nN\0\0\2\0\4\0\0056\0\0\0009\0\1\0005\1\3\0=\1\2\0K\0\1\0\1\0\1\vmethod\npopup!matchup_matchparen_offscreen\6g\bvim\0", "setup", "vim-matchup")
+time([[Setup for vim-matchup]], false)
+time([[packadd for vim-matchup]], true)
+vim.cmd [[packadd vim-matchup]]
+time([[packadd for vim-matchup]], false)
+-- Config for: toggleterm.nvim
+time([[Config for toggleterm.nvim]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
+time([[Config for toggleterm.nvim]], false)
 -- Config for: nvim-autopairs
 time([[Config for nvim-autopairs]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
 time([[Config for nvim-autopairs]], false)
+-- Load plugins in order defined by `after`
+time([[Sequenced loading]], true)
+vim.cmd [[ packadd nvim-lspconfig ]]
+vim.cmd [[ packadd lspsaga.nvim ]]
+
+-- Config for: lspsaga.nvim
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\flspsaga\frequire\0", "config", "lspsaga.nvim")
+
+time([[Sequenced loading]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
