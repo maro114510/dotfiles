a6f2a1be58ae24476b486f45f1b39009c1273e4d
maro114510 <atumaro114510@icloud.com>
2023-12-18 21:49:16 +0900

    ctrl+tでNerdTreeを起動するように修正

--- .config/nvim/lua/fff.lua
+++ .config/nvim/lua/fff.lua
@@ -1,32 +1,25 @@
-vim.api.nvim_set_keymap('n', '<leader>to', [[:lua require's:TabOpenFunc'()<CR>]], { noremap = true, silent = true })
-vim.cmd([[command! -nargs=0 FZFTabOpen lua require's:TabOpenFunc'()]])
 
-function _G.TabOpenFunc()
-    local tab_list = vim.fn.execute('tabs')
-    local text_list = {}
+local actions = require "fzf-lua.actions"
 
-    for _, tab_text in ipairs(vim.fn.split(tab_list, '\n')) do
-        local tab_page_text = string.match(tab_text, '^Tab page')
-        if not tab_page_text then
-            local page_num = string.match(tab_text, '[0-9]*$')
-            table.insert(text_list, string.format('%d %s', page_num, tab_text))
-        end
-    end
-
-    local result = vim.fn['fzf#run']({
-        source = text_list,
-        sink = function(line) _G.TabListSink(line) end,
-        options = '-m -x +s',
-        down = '40%',
-    })
-
-    if result then
-        vim.cmd('normal ' .. vim.fn.split(result, ' ')[1] .. 'gt')
-    end
+file_tabedit = function(selected, opts)
+	actions.file_tabedit(selected, opts)
+	vim.cmd("NERDTree")
+	vim.cmd("wincmd p")
+	-- actions.file_tabedit(selected, opts)
 end
 
-function _G.TabListSink(line)
-    local parts = vim.fn.split(line, '%s')
-    vim.cmd('normal ' .. parts[1] .. 'gt')
-end
+
+require'fzf-lua'.setup {
+	actions = {
+		files = {
+			["default"]     = actions.file_edit_or_qf,
+			["ctrl-t"]      = file_tabedit,
+			["ctrl-s"]      = actions.file_split,
+			["ctrl-v"]      = actions.file_vsplit,
+			["alt-q"]       = actions.file_sel_to_qf,
+			["alt-l"]       = actions.file_sel_to_ll,
+		},
+	},
+}
+
 
