d21ffd59bc6ec9bc31cb61f5795d24581e691588
maro114510 <atumaro1144510@icloud.com>
2023-12-18 16:51:50 +0900

    hlslensを削除

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -60,25 +60,6 @@ return require('packer').startup(function(use)
 		branch = 'release'
 	}
 
-	-- scroll bar
-	use 'petertriho/nvim-scrollbar'
-	use {
-		"kevinhwang91/nvim-hlslens",
-		config = function()
-			require("hlslens").setup({
-				build_position_cb = function(plist, _, _, _)
-					require("scrollbar.handlers.search").handler.show(plist.start_pos)
-				end,
-			})
-			vim.cmd([[
-				augroup scrollbar_search_hide
-					autocmd!
-					autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
-				augroup END
-			]])
-		end,
-	}
-
 	-- fzf
 	use {
 		'junegunn/fzf.vim',
