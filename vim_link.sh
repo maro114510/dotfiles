e7615974a81bc467edfe45bf99a0e4c7cf029b95
maro114510 <atumaro114510@icloud.com>
2023-12-20 02:13:50 +0900

    vimのリンク作成を修正

--- vim_link.sh
+++ vim_link.sh
@@ -7,25 +7,22 @@ NVIM_DIR="$HOME/.config/nvim"
 # neovim link
 if [ -d "$NVIM_DIR" ]; then
     BACKUP_DIR="$HOME/.config/nvim/nvim_backup_$(date +"%Y%m%d_%H%M%S")"
-    rsync -a "$NVIM_DIR" "$BACKUP_DIR"
+    cp -r "$NVIM_DIR" "$BACKUP_DIR" || { echo "nvim backup faild"; exit 1; }
     echo "nvim backup done"
 else
 	mkdir -p "$NVIM_DIR"
-	for item in "$TARGET_DIR"/*; do
-		if [ -e "$item" ]; then
-			ln -sfn "$item" "$NVIM_DIR/$(basename "$item")"
-		fi
-	done
+	ln -s "$TARGET_DIR"/* "$NVIM_DIR" || { echo "nvim link faild"; exit 1; }
 	echo "nvim linked"
 fi
 
 # vim link
-if [ -f "$CURRENT_DIR/.vimrc" ]; then
+if [ -f "$HOME/.vimrc" ]; then
     BACKUP_FILE="$HOME/.vimrc_backup_$(date +"%Y%m%d_%H%M%S")"
-	rsync -oL "$HOME/.vimrc" "$BACKUP_FILE"
+	cp -r "$HOME/.vimrc" "$BACKUP_FILE" || { echo "vim backup faild"; exit 1; }
 	echo ".vimrc backup done"
 else
-	ln -sfn "$CURRENT_DIR/.vimrc" "$HOME/.vimrc"
+	touch .vimrc
+	ln -s "$CURRENT_DIR/.vimrc" "$HOME/.vimrc" || { echo "vim link faild"; exit 1; }
 	echo ".vimrc linked"
 fi
 
