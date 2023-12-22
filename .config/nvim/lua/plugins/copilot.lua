c3f0c6a1eb51151cefc887d387a2b3fa7c47b9f0
maro114510 <atumaro1144510@icloud.com>
2023-12-22 13:16:13 +0900

    delete plugins.lua

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,18 @@
+
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		opts = function()
+			require("copilot").setup({})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		dependencies = { "copilot.lua" },
+		config = function ()
+			require("copilot_cmp").setup()
+		end
+	}
+}
