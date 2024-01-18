282c57b81c5645699672fa694149265e05fb4ccf
maro114510 <atumaro114510@icloud.com>
2024-01-18 16:59:32 +0900

    markdown

--- .config/nvim/after/plugin/notice.lua
+++ .config/nvim/after/plugin/notice.lua
@@ -7,8 +7,8 @@ noice.setup({
 	lsp = {
 		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
+			--["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			--["vim.lsp.util.stylize_markdown"] = true,
 			["cmp.entry.get_documentation"] = true,
 		},
 	},
