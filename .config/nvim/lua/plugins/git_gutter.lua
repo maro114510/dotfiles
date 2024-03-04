3dfa0630cd18d83e58d42cc77ba14d2d3b104967
maro114510 <atumaro114510@icloud.com>
2024-03-04 13:12:48 +0700

    change git-gutter vivid

--- .config/nvim/lua/plugins/git_gutter.lua
+++ .config/nvim/lua/plugins/git_gutter.lua
@@ -6,5 +6,10 @@ return {
 	event = "VimEnter",
 	config = function()
 		vim.cmd("GitGutterEnable")
+		-- vivid green
+		-- vim.cmd("highlight SignColumn guibg=NONE guifg=#00FF00")
+		vim.cmd("highlight GitGutterAdd guifg=#009900 ctermfg=2")
+		vim.cmd("highlight GitGutterChange guifg=#bbbb00 ctermfg=3")
+		vim.cmd("highlight GitGutterDelete guifg=#ff2222 ctermfg=1")
 	end,
 }
