c19b8856d6b0fc22ae5db4b5a8d4fb970d0de443
maro114510 <atumaro114510@icloud.com>
2024-05-08 19:51:55 +0900

    feat: add treesj

---
+++ .config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,17 @@
+-- https://github.com/Wansmer/treesj.git
+
+return {
+	'Wansmer/treesj',
+
+	keys = { '<space>m', '<space>j', '<space>s' },
+
+	dependencies = { 'nvim-treesitter/nvim-treesitter' },
+
+	config = function()
+		require('treesj').setup({--[[ your config ]]})
+	end,
+
+	cmd = {
+		"TSJToggle",
+	},
+}
