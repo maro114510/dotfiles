2bed81a1ad39ba0d5bd86d4e2c748e6adb43af7b
maro114510 <atumaro114510@icloud.com>
2025-01-05 23:24:48 +0900

    feat: Add notify

---
+++ .config/nvim/lua/plugins/nvim-notify.lua
@@ -0,0 +1,20 @@
+-- https://github.com/rcarriga/nvim-notify
+
+return {
+	'rcarriga/nvim-notify',
+
+	config = function()
+		local notify = require('notify')
+		notify.setup({
+			-- Animation style (see below for details)
+			animation_style = 'slide',
+
+			max_width = 50,
+			timeout = 1000,
+		})
+
+		vim.notify = notify
+	end,
+
+	event = 'UIEnter',
+}
