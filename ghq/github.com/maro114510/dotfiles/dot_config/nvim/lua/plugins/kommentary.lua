d8bc073c3426ec92f31c22dad2da476e760a94bd
maro114510 <atumaro114510@icloud.com>
2025-01-06 09:07:18 +0900

    Revert "chore: chezmoi ok"
    This reverts commit cd787f933263e77278eedf0e832389469587118c.

--- ghq/github.com/maro114510/dotfiles/dot_config/nvim/lua/plugins/kommentary.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/b3nj5m1n/kommentary.git
-
-return {
-	'b3nj5m1n/kommentary',
-
-	event = "VimEnter"
-}
--- ghq/github.com/maro114510/dotfiles/dot_config/nvim/lua/plugins/vim_wakatime.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/wakatime/vim-wakatime.git
-
-return {
-	"wakatime/vim-wakatime",
-	lazy=false,
-	setup=function()
-		vim.cmd([[packadd wakatime/vim-wakatime]])
-	end,
-}
