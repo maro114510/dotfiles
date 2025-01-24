103439cf1fdf1d285c1342a182f990af570a960b
maro114510 <atumaro114510@icloud.com>
2025-01-24 20:23:20 +0900

    feat: Impl copilot chat

---
+++ dot_config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,23 @@
+-- https://github.com/CopilotC-Nvim/CopilotChat.nvim.git
+
+vim.opt.splitright = true
+
+return {
+	"CopilotC-Nvim/CopilotChat.nvim",
+	dependencies = {
+		{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+		{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+	},
+	build = "make tiktoken", -- Only on MacOS or Linux
+	opts = {
+		window = {
+			layout = 'vertical',
+			width = 0.3,
+			height = 0.5, 
+		},
+	},
+	keys = {
+		{ "<C-x><C-x>", "<cmd>CopilotChat<CR>", mode = { "n", "v" } }
+	},
+	cmd = "CopilotChat",
+}
