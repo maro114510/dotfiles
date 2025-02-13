89320d3d8c843dbcd0c487e42439ee4c6441f77b
maro114510 <atumaro114510@icloud.com>
2025-02-14 08:07:32 +0900

    Update .config/nvim/lua/plugins/tiny-inline-diagnostic.lua

--- dot_config/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ dot_config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -3,8 +3,8 @@
 return {
 	"rachartier/tiny-inline-diagnostic.nvim",
 
-	event = "LspAttach", -- Or `LspAttach`
-	priority = 500, -- needs to be loaded in first
+	event = "VeryLazy", -- Or `LspAttach`
+	priority = 1000, -- needs to be loaded in first
 
 	config = function()
 		vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
