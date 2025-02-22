87febbca3413d117b3fddf6b21bd18bb5e407590
maro114510 <atumaro114510@icloud.com>
2025-02-23 02:22:03 +0900

    Update .config/nvim/lua/config/lspconfig.lua

--- dot_config/nvim/lua/config/lspconfig.lua
+++ dot_config/nvim/lua/config/lspconfig.lua
@@ -1,24 +1,23 @@
 local on_attach = function(client, bufnr)
 
-  -- LSPが持つフォーマット機能を無効化する
-  -- →例えばtsserverはデフォルトでフォーマット機能を提供しますが、利用したくない場合はコメントアウトを解除してください
-  --client.server_capabilities.documentFormattingProvider = false
+	-- LSPが持つフォーマット機能を無効化する
+	-- →例えばtsserverはデフォルトでフォーマット機能を提供しますが、利用したくない場合はコメントアウトを解除してください
+	--client.server_capabilities.documentFormattingProvider = false
 
-  -- 下記ではデフォルトのキーバインドを設定しています
-  -- ほかのLSPプラグインを使う場合（例：Lspsaga）は必要ないこともあります
+	-- 下記ではデフォルトのキーバインドを設定しています
+	-- ほかのLSPプラグインを使う場合（例：Lspsaga）は必要ないこともあります
 
-  local set = vim.keymap.set
-  set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-  set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-  set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-  set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-  set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-  set("n", "ma", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-  set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-  set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
-  set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
-  set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
-
+	local set = vim.keymap.set
+	set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+	set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+	set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+	set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+	set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+	set("n", "ma", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+	set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+	set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+	set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+	set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
 end
 
 -- 補完プラグインであるcmp_nvim_lspをLSPと連携させています（後述）
@@ -28,10 +27,16 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
-  function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {
-      on_attach = on_attach, --keyバインドなどの設定を登録
-      capabilities = capabilities, --cmpを連携
-    }
-  end,
+	function (server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup {
+			on_attach = on_attach, --keyバインドなどの設定を登録
+			capabilities = capabilities, --cmpを連携
+		}
+	end,
 }
+
+require("lspconfig").typos_lspsetup({
+	init_options = {
+		config = "~/.config/nvim/spell/.typos.toml",
+	},
+})
