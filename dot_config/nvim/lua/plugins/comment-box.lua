37c996b1b049707e0914d333ecaf0645a20b8422
maro114510 <atumaro114510@icloud.com>
2025-03-15 23:18:02 +0900

    Add .config/nvim/lua/plugins/comment-box.lua

---
+++ dot_config/nvim/lua/plugins/comment-box.lua
@@ -0,0 +1,10 @@
+-- https://github.com/LudoPinelli/comment-box.nvim.git
+
+return {
+	"LudoPinelli/comment-box.nvim",
+	cmd = { "CBccbox", "CBllline", "CBline" },
+	keys = {
+		{ "<leader>cb", "<Cmd>CBccbox<CR>", mode = { "n", "v" }, noremap = true, silent = true },
+		{ "<leader>ct", "<Cmd>CBllline<CR>", mode = { "n", "v" }, noremap = true, silent = true },
+	},
+}
