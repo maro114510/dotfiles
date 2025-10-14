db00d99c56933d522b43fa859291f63b068a641b
maro114510 <atsuki.nohira@fuller.co.jp>
2025-10-14 13:17:35 +0900

    Update .config/nvim/lua/user/ui.lua

--- dot_config/nvim/lua/user/ui.lua
+++ dot_config/nvim/lua/user/ui.lua
@@ -1,13 +1,13 @@
 local icons = require("user.icons")
 
-local signs = {
-	Error = icons.error_icon,
-	Warn = icons.warn_icon,
-	Hint = icons.hint_icon,
-	Info = icons.info_icon,
-	Todo = icons.todo_icon,
-}
-for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+-- Neovim 0.11+の新しいdiagnostic signs設定方式
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = icons.error_icon,
+			[vim.diagnostic.severity.WARN] = icons.warn_icon,
+			[vim.diagnostic.severity.HINT] = icons.hint_icon,
+			[vim.diagnostic.severity.INFO] = icons.info_icon,
+		}
+	}
+})
