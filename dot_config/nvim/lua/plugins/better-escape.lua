5cc6bb2d103a24d210b33fa760d71c8d3daab3f2
maro114510 <atsuki.nohira@fuller.co.jp>
2025-12-02 14:24:21 +0900

    Add .config/nvim/lua/plugins/better-escape.lua

---
+++ dot_config/nvim/lua/plugins/better-escape.lua
@@ -0,0 +1,18 @@
+-- https://github.com/max397574/better-escape.nvim
+
+return   {
+  "max397574/better-escape.nvim",
+
+  event = { "BufReadPre", "BufWritePre", "BufNewFile" },
+
+  opts = {
+    timeout = 100,
+    default_mappings = false,
+    mappings = {
+      i = { j = { k = "<ESC>" } },
+      t = {
+        j = { k = "<C-\\><C-n>" },
+      },
+    },
+  },
+}
