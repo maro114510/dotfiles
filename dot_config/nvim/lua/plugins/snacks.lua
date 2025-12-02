f9651762f49f06e7532f5fa72acc652d7b889d1c
maro114510 <atsuki.nohira@fuller.co.jp>
2025-12-02 14:19:09 +0900

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -101,7 +101,23 @@ return {
         },
       })
     end,
-    }
+    },
+    {
+      "<leader>lg",
+      mode = "n",
+      noremap = true,
+      desc = "Git: Open Lazygit",
+      function()
+        _G.__snacks_last_lg = require("snacks").lazygit.open()
+        _G._SNACKS_LG_CLOSE = function()
+          local last_lg = _G.__snacks_last_lg
+          if last_lg and last_lg.close then
+            pcall(last_lg.close, last_lg)
+          end
+          _G.__snacks_last_lg = nil
+        end
+      end,
+    },
   },
 
   init = function()
