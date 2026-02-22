b998f421520bf3a1705ce0022913b953a4519986
maro114510 <atsuki.nohira@stelzen.dev>
2026-02-22 14:36:47 +0900

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -11,5 +11,18 @@ vim.cmd([[
 	augroup END
 ]])
 
-vim.cmd([[highlight Normal ctermbg=NONE guibg=NONE]])
-vim.cmd([[highlight Comment cterm=italic gui=italic guifg=#ffd700 ctermfg=226]])
+local theme_override_group = vim.api.nvim_create_augroup("ThemeOverrides", { clear = true })
+
+local function apply_theme_overrides()
+	if vim.g.colors_name == "duskfox" then
+		vim.cmd([[highlight Normal ctermbg=NONE guibg=NONE]])
+		vim.cmd([[highlight Comment cterm=italic gui=italic guifg=#ffd700 ctermfg=226]])
+	end
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = theme_override_group,
+	callback = apply_theme_overrides,
+})
+
+apply_theme_overrides()
