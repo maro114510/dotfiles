970a4672d5d080437951207c4eb9f7e7e99d72b5
maro114510 <atsuki.nohira@kauche.com>
2026-04-28 10:39:46 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,38 +2,38 @@ vim.loader.enable()
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 local opts = {
-	defaults = {
-		lazy = true,
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				'gzip',
-				'matchit',
-				--'matchparen',
-				--'netrwPlugin',
-				'tarPlugin',
-				'tohtml',
-				'tutor',
-				'zipPlugin',
-			},
-		},
-	},
+  defaults = {
+    lazy = true,
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        --'matchparen',
+        --'netrwPlugin',
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
 }
 
 vim.opt.rtp:prepend(lazypath)
