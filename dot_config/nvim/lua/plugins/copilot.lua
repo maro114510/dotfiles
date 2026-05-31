c38a3c1bbe28bf27587aa69268c7be33fc5cbaca
maro114510 <atsuki.nohira@stelzen.dev>
2026-05-31 19:37:50 +0900

    Update .config/nvim/lua/plugins/copilot.lua

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,37 +1,42 @@
 -- https://github.com/github/copilot.vim.git
 
 return {
-  {
-    "github/copilot.vim",
-
-    event = "BufRead",
-  },
-
   -- {
-  --   "zbirenbaum/copilot.lua",
+  --   "github/copilot.vim",
   --
-  --   lazy = false,
-  --
-  --   dependencies = {
-  --     "copilotlsp-nvim/copilot-lsp",
-  --   },
-  --
-  --   config = function()
-  --     require("copilot").setup({
-  --       panel = {
-  --         enabled = false,
-  --       },
-  --       suggestion = {
-  --         enabled = true,
-  --         auto_trigger = true,
-  --       },
-  --       filetypes = { yaml = true, markdown = true, gitcommit = true, ["*"] = true },
-  --       keymaps = {
-  --         accept = "<C-l>",
-  --       },
-  --     })
+  --   init = function()
+  --     vim.g.copilot_node_command = vim.fn.expand("~/.local/share/mise/installs/node/latest/bin/node")
   --   end,
+  --
+  --   event = "BufRead",
   -- },
+
+  {
+    "zbirenbaum/copilot.lua",
+
+    event = "InsertEnter",
+    cmd = "Copilot",
+
+    dependencies = {
+      "copilotlsp-nvim/copilot-lsp",
+    },
+
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = false,
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<Tab>",
+          },
+        },
+        filetypes = { yaml = true, markdown = true, gitcommit = true, ["*"] = true },
+      })
+    end,
+  },
 
   -- {
   -- 	"zbirenbaum/copilot-cmp",
